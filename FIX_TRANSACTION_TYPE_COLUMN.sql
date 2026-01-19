63f39f5ccc3168ea5d4ef8f79570fb3b0ffad32d
Nikita Gurguts <nikitagurg@gmail.com>
2026-01-19 11:55:35 +0200

    Delete FIX_TRANSACTION_TYPE_COLUMN.sql

--- FIX_TRANSACTION_TYPE_COLUMN.sql
+++
@@ -1,7 +0,0 @@
--- Fix transaction type column size to support new transaction types
--- This script increases the size of the 'type' column to accommodate longer enum values
--- like EXTERNAL_INCOME, INTERNAL_TRANSFER, CURRENCY_CONVERSION, etc.
-
-ALTER TABLE `transactions` 
-    MODIFY COLUMN `type` VARCHAR(50) NOT NULL COMMENT 'Тип транзакции (расширен для новых типов)';
-
